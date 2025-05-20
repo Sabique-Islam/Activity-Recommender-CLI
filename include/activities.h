fb34e2b0c984acca2e43e25ad9faf6de6ff16a11
sabique <sabiqueislam7@gmail.com>
2025-05-21 01:04:23 +0530

    Activity Header to load activities.csv, calculate average vector from all tags, take user input, free the memory

---
+++ include/activities.h
@@ -0,0 +1,23 @@
+#ifndef ACTIVITIES_H
+#define ACTIVITIES_H
+
+#include "common.h"
+#include "embeddings.h"
+
+typedef struct {
+    char name[MAX_WORD_LEN];
+    char tags[MAX_TAGS][MAX_WORD_LEN];
+    float avg_vector[EMBEDDING_DIM];
+    int tag_count;
+} Activity;
+
+extern Activity* activities;
+extern int activity_count;
+extern int activities_capacity;
+
+void load_activities(const char* filename);
+void calculate_activity_vectors(HashMap* map);
+void process_group_preferences(HashMap* map);
+void cleanup_activities();
+
+#endif
