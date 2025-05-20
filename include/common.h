3dc98ed09aa9c406bbfd87f41c18f841d47d1c5b
sabique <sabiqueislam7@gmail.com>
2025-05-21 01:06:57 +0530

    Header file containing common header file includes and macro definition

---
+++ include/common.h
@@ -0,0 +1,21 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MAX_WORD_LEN 100
+#define MAX_LINE_LEN 8192
+#define EMBEDDING_DIM 300
+#define MAX_STOPWORDS 200
+#define HASH_SIZE 100003
+#define MAX_TAGS 10
+#define INITIAL_CAPACITY 1000
+#define GROWTH_FACTOR 2
+
+#endif
